bcb1b2abc5c71351d01640b2462bc575ec8bc86c
EncipherWizard <44020712+EncipherWizard@users.noreply.github.com>
2018-11-20 23:11:24 +0530

    Day 1 :: JDBC Connection

---
+++ Day1/src/p01/DeleteData.java
@@ -0,0 +1,36 @@
+package p01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteData {
+
+	public static void main(String[] args) {
+		try {
+			// load driver class
+			Class.forName("com.mysql.jdbc.Driver");
+			// creating connection
+			Connection con = DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/cdac_db", "cdac", "cdac");
+			// creating query statement
+			Statement s = con.createStatement();
+			// execute query
+			String q = "delete from student where rno = 103";
+			boolean b = s.execute(q);
+			System.out.println(b);
+			// closing connection
+			con.close();
+		
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+}
